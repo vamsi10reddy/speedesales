9b2d12c13d03e9693d80a3fc39feba60df1437b8
Vamsi Reddy <vamsi@Vamsis-MacBook-Air.local>
2024-10-26 12:52:19 -0500

    added sign-in.css, sign-in.html, implemented logout feature.

--- userauths/urls.py
+++ userauths/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from userauths.views import registration_page
+from userauths.views import registration_page, login_page, logout_page
 
 app_name = 'userauths'
 
 urlpatterns = [
     path("sign-up/", registration_page, name='registration_page'),  # Make sure to use the view function, not a string
+    path("sign-in/", login_page, name='login_page'),
+    path("sign-out/", logout_page, name='sign-out'),
 ]
--- userauths/views.py
+++ userauths/views.py
@@ -4,6 +4,10 @@ from userauths.forms import UserRegisterForm
 from django .contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.shortcuts import redirect
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+
+User = settings.AUTH_USER_MODEL
 
 def registration_page(request):
 
@@ -26,3 +30,36 @@ def registration_page(request):
         'form': form
     }
     return render(request, 'userauths/sign-up.html', context)
+
+def login_page(request):
+    if not request.user.is_authenticated and 'next' in request.GET:
+        messages.error(request, "You must be logged in to log out.")  # Error message for unauthorized logout attempt
+
+    if request.user.is_authenticated:
+        messages.warning(request, f"Hey you're already logged in.")
+        return redirect("core:index")  # Redirect if already authenticated
+
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=email, password=password)  # Authenticate using email
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Successfully logged in.")
+            return redirect("core:index")
+        else:
+            # Assuming email is not found or password is incorrect
+            messages.error(request, "Invalid email or password. Please try again.")
+
+    context = {
+
+    }
+    return render(request, "userauths/sign-in.html")
+
+@login_required(login_url='userauths:login_page')  # Redirects to login page if not logged in.
+def logout_page(request):
+    logout(request)
+    messages.success(request, "Logged Out Successfully.")
+    return redirect("userauths:login_page")
